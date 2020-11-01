0c38487495f6d964ab4afe4c7c33b1befaf7c7ec
Kateitsme <56411381+Kateitsme@users.noreply.github.com>
2020-11-01 16:51:44 +0300

    8

--- src/main/java/homework_8/Task_1.java
+++ src/main/java/homework_8/Task_1.java
@@ -6,22 +6,20 @@ import java.util.Set;
 import java.util.*;
 
 public class Task_1 {
-    public static void main(String[] args){
-        List<String> artists = Arrays.asList(
-                "Nirvana", "twice", "bts", "Beatles", "Queen",
+    public static Map<String, Integer> wordCount(List<String> words) {
+        Map<String, Integer> wordCount = new HashMap<>();
+
+        for (String word : words) {
+            wordCount.merge(word, 1, Integer::sum);
+        }
+
+        return wordCount;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(wordCount(Arrays.asList("Nirvana", "twice", "bts", "Beatles", "Queen",
                 "Coldplay", "Halsey", "Imagine Dragons", "Beatles", "Coldplay",
                 "AC/DC", "Madonna", "Eminem", "Ariana Grande", "BlackPink",
-                "Nirvana", "Eminem", "Halsey", "Nirvana", "Beatles"
-        );
-
-        System.out.println(artists.toString());
-        Set<String> unique = new HashSet<String>(artists);
-
-        System.out.println("Уникальные артисты");
-        System.out.println(unique.toString());
-        System.out.println("Частота встречаемости");
-        for (String artist : unique) System.out.println(artist + ": " + Collections.frequency(artists, artist));
-
-
+                "Nirvana", "Eminem", "Halsey", "Nirvana", "Beatles")));
     }
 }
